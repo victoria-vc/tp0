2287a74fab2a59eeff8d425c900620f21feb7444
victoria castagno <vcastagno@frba.utn.edu.ar>
2024-04-14 15:58:02 -0300

    arregle una letra

--- client/src/utils.c
+++ client/src/utils.c
@@ -30,7 +30,7 @@ int crear_conexion(char *ip, char* puerto)
 
 	// Ahora vamos a crear el socket.
 	// int socket_cliente = 0;
-	int socket_cliente = socket(srver_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 	
 	// Ahora que tenemos el socket, vamos a conectarlo
 	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
